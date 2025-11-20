89478a24fa9c404eb55b0562655a7484b2587158
M S I <M S I@MSI>
2025-11-20 17:07:23 +0700

    Menambahkan kerangka Scaffold dan TopAppBar.

--- app/src/main/java/com/example/myarsitekturmvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/TampilSiswa.kt
@@ -32,3 +32,26 @@ fun TampilSiswa(
     statusUiSiswa: Siswa,
     onBackButtonClicked:()->Unit
 ){
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama), second = statusUiSiswa.nama),
+        Pair(first = stringResource(id = R.string.gender), second = statusUiSiswa.gender),
+        Pair(first = stringResource(id = R.string.alamat), second = statusUiSiswa.alamat)
+    )
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id=R.string.detail),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource
+                        (id = R.color.purple_500)
+                    )
+            )
+        }){     isiRuang->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {

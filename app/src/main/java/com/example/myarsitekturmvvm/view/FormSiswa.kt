89bda0441fa0cc552f5b39edb749758fbc3535aa
M S I <M S I@MSI>
2025-11-20 16:46:07 +0700

    menambahkan pilihan radio button

--- app/src/main/java/com/example/myarsitekturmvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/FormSiswa.kt
@@ -86,3 +86,28 @@ fun FormIsian(
                     .width(250.dp), thickness = Thickness, color =
                     Color.Red
             )
+            Row {
+                pilihanJK.forEach { item ->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender ==item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                        verticalAlignment =  Alignment.CenterVertically) {
+                        RadioButton(
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )

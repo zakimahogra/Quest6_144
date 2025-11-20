c42bdee7b13b215d756b3953ca70d145c1280840
M S I <M S I@MSI>
2025-11-20 16:36:12 +0700

    menambahkan string si dataJK

---
+++ app/src/main/java/com/example/myarsitekturmvvm/model/DataJK.kt
@@ -0,0 +1,11 @@
+package com.example.myarsitekturmvvm.model
+
+import com.example.myarsitekturmvvm.R
+
+object DataJK {
+    val JenisK= listOf(
+        R.string.laki,
+        R.string.wanita
+
+    )
+}
